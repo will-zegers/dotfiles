7f216ed0acac02287c4e5025f56a2deb3c4e6763
Will Zegers <will.zegers@gmail.com>
2020-02-22 17:05:41 -0800

    first commit

---
+++ .vimrc
@@ -0,0 +1,6 @@
+set colorcolumn=80
+set cursorline
+set expandtab
+set number
+set relativenumber
+set tabstop=4
---
+++ configure_system.sh
@@ -0,0 +1,67 @@
+#! /usr/bin/env bash
+
+set -e
+
+CONFIG_WITNESS=${HOME}/.config/config_witness
+
+if [ -f ${CONFIG_WITNESS} ]
+then
+  exit 0
+fi
+
+CONFIG_FILES=${HOME}/{${HOME}/.config/terminator/config,${HOME}/.vimrc,${HOME}/.zshrc}
+sudo -v
+
+rm -rf ${HOME}/{Documents,Music,Pictures,Public,Templates,Videos}
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install \
+  build-essential \
+  cmatrix \
+  cowsay \
+  curl \
+  ddate \
+  fortune \
+  git \
+  ipython \
+  lolcat \
+  python3-venv \
+  python3-pip \
+  terminator \
+  tmux \
+  vim \
+  zsh \
+;
+sudo apt -y autoremove
+
+# ZSH tools and plugins
+## Oh My Zsh
+DIR=${HOME}/.oh-my-zsh
+if [ ! -d ${DIR} ]
+then
+  echo ${DIR}
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+## zsh-autosuggestion
+DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d ${DIR} ]
+then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${DIR}
+fi
+
+# SSH setup
+KEY_TYPE=ed25519
+if [ ! -f ${HOME}/.ssh/id_${KEY_TYPE}.pub ]
+then
+  ssh-keygen -t ${KEY_TYPE}
+fi
+
+# Use Python3
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+
+cp ./config.terminator ${HOME}/.config/terminator/config
+cp ./{.vimrc,.zshrc} ${HOME}
+
+touch ${CONFIG_WITNESS}
