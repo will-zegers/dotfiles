4e8062f1f80ceb3056b91f796fe41056b00999e0
Will Zegers <will.zegers@gmail.com>
2020-10-24 04:42:26 +0000

    Customizable theme

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 function add-dotfile-as-source()
 {
     local src=$1
@@ -19,6 +21,7 @@ OH_MY_ZSH_DIR=${HOME}/.oh-my-zsh
 if [ ! -d ${OH_MY_ZSH_DIR} ]
 then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+  cp ${SCRIPT_PATH}/zsh/themes/* ${OH_MY_ZSH_DIR}/themes/
 fi
 
 ## zsh-autosuggestions
@@ -28,7 +31,6 @@ then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_AUTOSUGGESTIONS_DIR}
 fi
 
-SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 add-dotfile-as-source ${SCRIPT_PATH}/bash/.bashrc ${HOME}/.bashrc
 add-dotfile-as-source ${SCRIPT_PATH}/tmux/.tmux.conf ${HOME}/.tmux.conf
 add-dotfile-as-source ${SCRIPT_PATH}/vim/.vimrc ${HOME}/.vimrc
