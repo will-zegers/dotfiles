80bd116f10c07764f330258774861821e294a7e7
Will Zegers <will.zegers@gmail.com>
2026-01-31 19:27:53 -0800

    LSP support for Neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,3 +11,7 @@ vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 -- Prefer normal escape sequences in terminal mode over <C-\><C-n>
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("t", "<C-[>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
